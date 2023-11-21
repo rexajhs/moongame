b5bd99f9669f62e859943c2a67203bb6f3f4906e
rexajhs <151103500+rexajhs@users.noreply.github.com>
2023-11-21 14:34:48 +1300

    Update main.py
    fixed bug

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 letter = letters[random.randint(1,25)]
 def check(x):
     x = x.lower()
     return x.startswith(letter)
 def bring():
-    return letter+letters[random.randint(1,25)]+letters[random.randint(1,25)]
+    return letter+letters[random.randint(0,25)]+letters[random.randint(0,25)]
 
 print('GOING TO THE MOON GAME')
 while 1:
