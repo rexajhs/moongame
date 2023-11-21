a9325c62d41164d4d9a3761a828f8d58d7089eef
rexajhs <151103500+rexajhs@users.noreply.github.com>
2023-11-21 15:01:04 +1300

    Update main.py
    better

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-letter = letters[random.randint(1,25)]
-def check(x):
-    x = x.lower()
-    return x.startswith(letter)
-def bring():
-    return letter+letters[random.randint(0,25)]+letters[random.randint(0,25)]
+letter = letters[random.randint(0,25)]
 
-print('GOING TO THE MOON GAME')
+def check(item):
+    item = item.lower()
+    return item.startswith(letter)
+def bring():
+    word = letter
+    word += letters[random.randint(0,25)]
+    word += letters[random.randint(0,25)]
+    return word
+
+print("GOING TO THE MOON GAME")
+print("I will bring " + bring() + "!")
 while 1:
-    item = input('Type in your item: ')
-    if(check(item)):
-        print(f"You can bring {item}.")
+    item = input("What will you bring? ")
+    if (check(item)):
+        print("You can bring " + item + "!")
         break
     else:
-        print(f"You can't bring {item}.")
-        print(f"I will bring {bring()}.")
+        print("You can't bring " + item + "!")
+        print("I will bring " + bring() + ".")
+        
         
